37f753cb46117a4a82433151bbf64b8c60c50440
JatinNeeraj <jatinucm123@gmail.com>
2022-11-14 17:37:24 +0530

    Sreenivas

---
+++ Projects/DocumentMan/controllers/DocumentDetails.controller.js
@@ -0,0 +1,17 @@
+//let CommonDataSupply = require("../../../DataSupply/Fs/Config/Folders/Files/PullData/FromConfigFolder/FromDisplayJson/AsJson")
+let CommonDataSupply = require("../../../DataSupply/Fs/Config/Folders/Files/PullData/FromDataFolder/FromFolderAndFile")
+
+let getDocumentDetails = async (req, res) => {
+    let jVarLocalInfileName = req.body.inFileName;
+
+    let LocalFromCommonFromDataSupply = await CommonDataSupply.FullJsonData({
+        inDataPK: 2051,
+        inFolderName: "Loans",
+        inFileNameWithExtension: jVarLocalInfileName
+    });
+    console.log("LocalFromCommonFromDataSupply.KResult---",LocalFromCommonFromDataSupply);
+
+    res.json(LocalFromCommonFromDataSupply.KResult);
+};
+
+module.exports = { getDocumentDetails };
