ad466149005eabbac4a7b12ddbb572a6f2d589ae
Keshav <40142486+keshavsoft@users.noreply.github.com>
2022-11-14 14:56:08 +0530

    keshav

--- public/DocumentMan/Page/Js/ShowData.js
+++ public/DocumentMan/Page/Js/ShowData.js
@@ -1,27 +1,5 @@
 let jFShowToDOM = () => {
-
-    let jFShowToDOMNameOfBranchId = () => {
-        let jVarLocalKey = "BranchId";
-        let jVarLocalBranchId = document.getElementById(jVarLocalKey);
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalBranchId.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfBranchId();
-
-    let jFShowToDOMNameOfPACA = () => {
-        let jVarLocalKey = "NameOfPACA";
-        let jVarLocalNameId = document.getElementById("NameId");
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalNameId.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfPACA();
 
     let jFShowToDOMNameOfNumberId = () => {
@@ -48,68 +26,92 @@ let jFShowToDOM = () => {
     };
     jFShowToDOMNameOfPurposeId();
 
-    let jFShowToDOMNameOfName3Id = () => {
-        let jVarLocalKey = "Name3";
-        let jVarLocalName3Id = document.getElementById("Name3Id");
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("Name3");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalName3Id.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfName3Id();
 
-    let jFShowToDOMNameOfLandId = () => {
-        let jVarLocalKey = "Land";
-        let jVarLocalLand = document.getElementById("LandId");
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("Land");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalLand.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfLandId();
-
-    let jFShowToDOMNameOfVillageId = () => {
-        let jVarLocalKey = "Village";
-        let jVarLocalVillage = document.getElementById("VillageId");
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("Village");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalVillage.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfVillageId();
-
-    let jFShowToDOMNameOfName2Id = () => {
-        let jVarLocalKey = "Name2Id";
-        let jVarLocalName2Id = document.getElementById("Name2Id");
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("Name2Id");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalName2Id.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfName2Id();
-
-    let jFShowToDOMNameOfName1 = () => {
-        let jVarLocalKey = "Name1";
-        let jVarLocalName1 = document.getElementById("Name1Id");
-
-        let jVarLocalFromLocalstorage = localStorage.getItem("Name1");
-        let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
-
-        jVarLocalName1.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
-
-    };
     jFShowToDOMNameOfName1();
 
 };
 
+let jFShowToDOMNameOfVillageId = () => {
+    let jVarLocalKey = "Village";
+    let jVarLocalVillage = document.getElementById("VillageId");
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalVillage.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+
+};
+
+let jFShowToDOMNameOfName2Id = () => {
+    let jVarLocalKey = "Name2Id";
+    let jVarLocalName2Id = document.getElementById("Name2Id");
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalName2Id.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+
+};
+
+let jFShowToDOMNameOfLandId = () => {
+    let jVarLocalKey = "Land";
+    let jVarLocalLand = document.getElementById("LandId");
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalLand.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+
+};
+
+let jFShowToDOMNameOfBranchId = () => {
+    let jVarLocalKey = "BranchId";
+    let jVarLocalBranchId = document.getElementById(jVarLocalKey);
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalBranchId.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+
+};
+
+let jFShowToDOMNameOfPACA = () => {
+    let jVarLocalKey = "NameOfPACA";
+    let jVarLocalNameId = document.getElementById("NameId");
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalNameId.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+
+};
+
+let jFShowToDOMNameOfName1 = () => {
+    let jVarLocalKey = "Name1";
+    let jVarLocalName1 = document.getElementById("Name1Id");
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalName1.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+
+};
+
+let jFShowToDOMNameOfName3Id = () => {
+    let jVarLocalKey = "Name3";
+    let jVarLocalName3Id = document.getElementById("Name3Id");
+
+    let jVarLocalFromLocalstorage = localStorage.getItem("PageInfo");
+    let jVarLocalFromLocalstorageJson = JSON.parse(jVarLocalFromLocalstorage);
+
+    jVarLocalName3Id.innerHTML = jVarLocalFromLocalstorageJson[jVarLocalKey];
+};
+
+
 export {
     jFShowToDOM
-}
+};
