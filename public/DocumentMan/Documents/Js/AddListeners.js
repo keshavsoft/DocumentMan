d4d76b3b029e5ade22fe6736931429e0f6a1a0ca
JatinNeeraj <jatinucm123@gmail.com>
2022-11-14 18:20:41 +0530

    Sreenivas

--- public/DocumentMan/Documents/Js/AddListeners.js
+++ public/DocumentMan/Documents/Js/AddListeners.js
@@ -9,5 +9,25 @@ let StartFunc = () => {
     document.addEventListener("DOMContentLoaded", FromHbs);
 };
 
+let StartFuncShow = async () => {
+    let jVarLocalClassName = document.getElementsByClassName("ShowDocumentClass");
 
-export { StartFunc }
+    for (let i = 0; i < jVarLocalClassName.length; i++) {
+        jVarLocalClassName[i].addEventListener("click", function (event) {
+            let jVarLocalCurrentTarget = event.currentTarget;
+            let jvarFileName = jVarLocalCurrentTarget.dataset.filename;
+            console.log("jvarFileName----------------------------");
+
+            // document.location=`../Page/page1.html?LoanNumber=${jvarFileName}`;
+            document.location=`../Page/page1Show.html?DocumentNumber=${jvarFileName}`
+
+
+            console.log("llllllllllll--", jvarFileName);
+        })
+    };
+};
+
+
+
+
+export { StartFunc,StartFuncShow }
