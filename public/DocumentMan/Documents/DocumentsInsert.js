2061e694b63358e04cabfa7042a11c2a3437cdce
JatinNeeraj <jatinucm123@gmail.com>
2022-11-16 15:47:23 +0530

    Sreenivas

--- public/DocumentMan/Documents/DocumentsInsert.js
+++ public/DocumentMan/Documents/DocumentsInsert.js
@@ -3,7 +3,15 @@ import { getAll as FromLoansgetAll } from "./Js/FromLoans/ShowLoans.js";
 import { StartFunc as AddListenersStartFunc } from "./Js/AddListeners.js";
 
 FromLoansgetAll().then(PromiseData => {
-    console.log("PromiseData : ", PromiseData);
+
+    let Datalist = '';
+    let htmlstring = PromiseData.forEach((ListItems) => {
+        Datalist += '<option value="' + ListItems + '" />';
+    });
+
+    document.getElementById("DocumentList").innerHTML = Datalist;
+
+    console.log("PromiseData :--------- ", PromiseData);
 });
 
 AddListenersStartFunc();
