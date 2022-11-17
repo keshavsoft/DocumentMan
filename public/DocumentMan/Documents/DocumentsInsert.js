7202b3539d8d1c7670954d498eb99942663ce64f
JatinNeeraj <jatinucm123@gmail.com>
2022-11-17 19:30:57 +0530

    Sreenivas

--- public/DocumentMan/Documents/DocumentsInsert.js
+++ public/DocumentMan/Documents/DocumentsInsert.js
@@ -1,11 +1,18 @@
 import { getAll as FromLoansgetAll } from "./Js/FromLoans/ShowLoans.js";
 import { StartFunc as AddListenersStartFunc } from "./Js/AddListeners.js";
+import { ReturnLoanNumber } from "./Js/urlSearchParams.js";
+import {ShowInDom  } from "./Js/ToDom/LoanRef.js";
 
-//let jVarLocalLoanNumber = ReturnLoanNumber();
+let jVarLocalLoanNumber = ReturnLoanNumber();
+
+ShowInDom({inLoanRef:jVarLocalLoanNumber});
+
+console.log("jVarLocalLoanNumber--",jVarLocalLoanNumber);
+
 
 FromLoansgetAll().then(PromiseData => {
     let Datalist = '';
-    
+
     PromiseData.forEach((ListItems) => {
         Datalist += '<option value="' + ListItems + '" />';
     });
