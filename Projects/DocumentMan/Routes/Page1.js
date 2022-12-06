ac7eb99b8ca07031856d9dd9586197969e70eea7
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2022-12-06 20:11:52 +0530

    Sreenivas111

--- Projects/DocumentMan/Routes/Page1.js
+++ Projects/DocumentMan/Routes/Page1.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 let Controller = require("../controllers/page1.controller");
+let Controller = require("../controllers/page1.controller");
 
 router.post('/', Controller.CreatePage);
+router.PATCH('/Update', Controller.UpdateDocumentDetails);
 
 module.exports = router;
